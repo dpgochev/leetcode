cd02bf449f616fd23e40ce210c2a8d03d0c0e1df
Dimitar <dimitargotchev@gmail.com>
2025-02-23 20:32:18 +0200

    Add inital solutions for two_sum.

---
+++ two_sum/solution1.py
@@ -0,0 +1,9 @@
+# O(n^2)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            first_index=i
+            for j in range(i+1,len(nums)):
+                second_index=j
+                if nums[first_index]+nums[second_index] == target:
+                    return [first_index,second_index]
---
+++ two_sum/solution2.py
@@ -0,0 +1,34 @@
+# O(nlgn)
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_sorted = nums.copy()
+        # O(nlgn) goes here
+        nums_sorted.sort()
+        
+        # This should be O(n) at the worst case
+        while True:
+            nums_len = len(nums_sorted)
+            
+            min_index = 0
+            max_index = nums_len-1
+            min_el = nums_sorted[min_index]
+            max_el = nums_sorted[max_index]
+
+            if (min_el+max_el)==target:
+                if min_el==max_el:
+                    first_index = nums.index(min_el)
+                    second_index = nums.index(max_el,first_index+1)
+                else:
+                    first_index = nums.index(min_el)
+                    second_index = nums.index(max_el)
+                return [first_index,second_index]
+
+            elif (min_el+max_el)>target:
+                nums_sorted = nums_sorted[min_index:max_index]
+                continue
+            elif (min_el+max_el)<target:
+                nums_sorted = nums_sorted[min_index+1:max_index+1]
+                continue
+           
+        return []
