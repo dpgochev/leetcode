27880f5f719ae80313c97099cfba9fb9ebb0d38c
Dimitar <dimitargotchev@gmail.com>
2025-03-30 17:41:24 +0300

    Lint two_sum solution1.py

--- two_sum/solution1.py
+++ two_sum/solution1.py
@@ -2,11 +2,12 @@
 
 from typing import List
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            first_index=i
-            for j in range(i+1,len(nums)):
-                second_index=j
-                if nums[first_index]+nums[second_index] == target:
-                    return [first_index,second_index]
+            first_index = i
+            for j in range(i + 1, len(nums)):
+                second_index = j
+                if nums[first_index] + nums[second_index] == target:
+                    return [first_index, second_index]
